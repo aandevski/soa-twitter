0fa19f5c5d51f1f061023d4d00b26d10919d70fd
Aleksandar Andevski <a.andevski@gmail.com>
2019-07-07 14:57:41 +0200

    Add address & password to services

--- tweets/consul.py
+++ tweets/consul.py
@@ -1,12 +1,7 @@
-import sys
 import consulate
 import os
 
 
 def register():
     consul = consulate.Consul(host=os.environ['CONSUL_HOST'], port=os.environ['CONSUL_PORT'])
-
-    for i, arg in enumerate(sys.argv):
-        if arg == '-b':
-            port = int(sys.argv[i+1][1:])
-            consul.agent.service.register(name='tweets', port=port)
+    consul.agent.service.register(name='tweets', port=int(os.environ['SERVICE_PORT']), address=os.environ['SERVICE_IP'])
--- users/consul.py
+++ users/consul.py
@@ -1,12 +1,7 @@
-import sys
 import consulate
 import os
 
 
 def register():
     consul = consulate.Consul(host=os.environ['CONSUL_HOST'], port=os.environ['CONSUL_PORT'])
-
-    for i, arg in enumerate(sys.argv):
-        if arg == '-b':
-            port = int(sys.argv[i+1][1:])
-            consul.agent.service.register(name='users', port=port)
+    consul.agent.service.register(name='users', port=int(os.environ['SERVICE_PORT']), address=os.environ['SERVICE_IP'])
