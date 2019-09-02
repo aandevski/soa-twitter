31d8d8a0f1bf87a25c0f41717449d28fb1b0ce39
cilcil97 <filipspasovski997@gmail.com>
2019-09-02 23:25:11 +0200

    WIP

--- tweets/app.py
+++ tweets/app.py
@@ -1,11 +1,70 @@
+import os
 from flask import Flask
 import consul
+
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# consul.register()
+
+
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-consul.register()
+from models import Tweet
 
 
 @app.route("/")
 def hello():
-    return "Hello from tweets!"
+    return "Hello from Tweets!"
+
+
+@app.route("/add",methods=['POST','GET'])
+def add_tweet():
+    name = request.args.get('name')
+    author = request.args.get('author')
+    published = request.args.get('published')
+    try:
+        tweet = Tweet(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(tweet)
+        db.session.commit()
+        return "Tweet added. tweet id={}".format(tweet.id, tweet.author,tweet.name)
+    except Exception as e:
+        return (str(e))
+
+
+@app.route("/getall")
+def get_all():
+    try:
+        tweets = Tweet.query.all()
+        return jsonify([e.serialize() for e in tweets])
+    except Exception as e:
+        return (str(e))
+
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        tweet = Tweet.query.filter_by(id=id_).first()
+        return jsonify(tweet.serialize())
+    except Exception as e:
+        return (str(e))
+
+
+if __name__ == '__main__':
+    app.run()
