48bc33a1f72811bdf946b83e1e13c2c1a0db6002
Aleksandar Andevski <a.andevski@gmail.com>
2019-09-18 01:02:07 +0200

    Add auth to users service

--- install.sh
+++ install.sh
@@ -7,41 +7,35 @@ curl -i -X POST \
   --url http://$1/services/ \
   --data 'name=tweets' \
   --data 'url=http://tweets.service.consul'
-
 curl -i -X POST \
   --url http://$1/services/tweets/routes \
   --data 'paths[]=/tweets'
-
-curl -i -X POST \
-  --url http://$1/services/tweets/routes \
-  --data 'paths[]=/tweets'
-
-
 
 
 curl -i -X POST \
   --url http://$1/services/ \
   --data 'name=users' \
   --data 'url=http://users.service.consul'
-
 curl -i -X POST \
   --url http://$1/services/users/routes \
   --data 'paths[]=/users'
+curl -X POST http://$1/services/users/plugins \
+  --data "name=key-auth"
+
 
 curl -i -X POST \
   --url http://$1/services/ \
   --data 'name=follows' \
   --data 'url=http://follows.service.consul'
-
 curl -i -X POST \
   --url http://$1/services/follows/routes \
   --data 'paths[]=/follows'
+
 
 curl -i -X POST \
   --url http://$1/services/ \
   --data 'name=favorites' \
   --data 'url=http://favorites.service.consul'
-
 curl -i -X POST \
   --url http://$1/services/favorites/routes \
   --data 'paths[]=/favorites'
