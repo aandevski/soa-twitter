e29bda3f486800b0a8f582d9b7e93cb042a00734
Aleksandar Andevski <a.andevski@gmail.com>
2019-09-17 22:25:19 +0200

    Add default config option for development

--- users/manage.py
+++ users/manage.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig'))
 
 migrate = Migrate(app, db)
 manager = Manager(app)
