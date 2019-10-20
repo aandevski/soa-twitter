9a5f5a92948508c839a97705d9bc50545c0d868c
Aleksandar Andevski <a.andevski@gmail.com>
2019-10-20 20:17:58 +0200

    Inter-process communication using kong

--- follows/consul.py
+++ follows/consul.py
@@ -1,6 +1,5 @@
 import consulate
 import os
-import yaml
 
 consul = consulate.Consul(host=os.environ['CONSUL_HOST'], port=os.environ['CONSUL_PORT'])
 
@@ -10,12 +9,3 @@ def register():
     port = os.environ['SERVICE_PORT']
     service_name = os.environ['SERVICE_NAME']
     consul.agent.service.register(name=service_name, port=int(port), address=host)
-    service_info = {
-        'host': host,
-        'port': port
-    }
-    consul.kv.set(service_name, service_info)
-
-
-def get_value(key):
-    return yaml.safe_load(consul.kv.get(key))
--- follows/service.py
+++ follows/service.py
@@ -1,15 +1,12 @@
 import os
-import consul
 import requests
 
-users_service = os.environ['USERS_SERVICE_NAME']
+kong_host = os.environ.get('KONG_HOST')
+kong_port = os.environ.get('KONG_PORT')
 
 
 def check_user_existence(user_id):
-    service_data = consul.get_value(users_service)
-    host = service_data.get('host')
-    port = service_data.get('port')
-    request_url = f"http://{host}:{port}/{user_id}"
+    request_url = f"http://{kong_host}:{kong_port}/users/{user_id}"
     reply = requests.get(request_url)
     if reply.status_code != requests.status_codes.codes.ok:
         raise Exception('User with id={} does not exist'.format(user_id))
